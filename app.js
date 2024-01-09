d23b5b5e8ea81b34c04ff05bf33af32904500186
Logan Hanaway <lthanaway14@gmail.com>
2024-01-09 02:00:12 -0600

    local storage added

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const form = document.querySelector('#addTodo');
 const input = document.querySelector('#newTodo');
 const todoList = document.querySelector('#todoList')
 
+document.addEventListener('DOMContentLoaded', function() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+        todoList.innerHTML = storedTodos;
+    }
+});
 
 todoList.addEventListener('click', function(e){
     if (e.target.id === 'remove'){
         e.target.parentElement.remove();
+        updateLocalStorage();
     } else if (e.target.id === 'complete'){
         e.target.parentElement.classList.toggle('completed');
+        updateLocalStorage();
     }
 });
 
@@ -25,5 +33,10 @@ form.addEventListener('submit', function(e){
     newTodo.appendChild(completeBtn);
     newTodo.appendChild(removeBtn);
     todoList.appendChild(newTodo);
+    updateLocalStorage();
 });
 
+function updateLocalStorage() {
+    const todos = todoList.innerHTML;
+    localStorage.setItem('todos', todos);
+}
